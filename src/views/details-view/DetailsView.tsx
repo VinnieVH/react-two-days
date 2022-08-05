c8d442b1675bf65821aa0bda4b2059422660140e
Joris Opsommer <joris.opsommer@realdolmen.com>
2022-08-05 15:55:39 +0200

    usestate & useeffect

--- src/views/details-view/DetailsView.tsx
+++ src/views/details-view/DetailsView.tsx
@@ -1,8 +1,17 @@
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const DetailsView = () => {
-    const {id} = useParams();
-    return <div>
-        <h3>ID: {id}</h3>
+  const { id } = useParams();
+  const [coinId, setCoinId] = useState(id || "");
+
+  useEffect(() => {
+    id && setCoinId(id);
+  }, [id]);
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
     </div>
-}
+  );
+};
