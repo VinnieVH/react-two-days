189403ab4d60283ee51361c59332b2e4891097f8
Joris Opsommer <joris.opsommer@realdolmen.com>
2022-08-05 14:41:58 +0200

    styled-components nav

--- src/views/nav/Nav.tsx
+++ src/views/nav/Nav.tsx
@@ -1,47 +1,49 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Nav = () => {
   const navigate = useNavigate();
 
-  const appBarStyle = {
-    height: "60px",
-    color: "white",
-    backgroundColor: "#1976D2",
-  };
-  const appBarFlexStyle = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "100%",
-  };
-  const itemStyle = {
-    width: "200px",
-    display: "flex",
-    justifyContent: "center",
-    fontWeight: 500,
-    fontSize: "15px",
-    margin: "1rem",
-  };
   return (
-    <div style={appBarStyle}>
-      <div style={appBarFlexStyle}>
-        <div style={itemStyle}>
+    <AppBar>
+      <AppBarFlex>
+        <AppBarItem>
           <img
             alt="logo coinmarketcap"
             src="https://assets-global.website-files.com/5f15b50525745912903311ad/5f15b505dd58c115cd49d484_Logo-full-white.svg"
             width={150}
             onClick={() => navigate("/")}
           />
-        </div>
-        <div style={itemStyle} onClick={() => navigate("/")}>
-          CRYPTOCURRENCIES
-        </div>
-        <div style={{ ...itemStyle, justifyContent: "flex-end" }} onClick={() => navigate("/")}>
+        </AppBarItem>
+        <AppBarItem onClick={() => navigate("/")}>CRYPTOCURRENCIES</AppBarItem>
+        <AppBarItem style={{ justifyContent: "flex-end" }} onClick={() => navigate("/")}>
           WATCHLIST
-        </div>
-      </div>
-    </div>
+        </AppBarItem>
+      </AppBarFlex>
+    </AppBar>
   );
 };
 
 export default Nav;
+
+const AppBar = styled.div`
+  height: 60px;
+  color: white;
+  background-color: #1976d2;
+`;
+
+const AppBarFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+`;
+
+const AppBarItem = styled.div`
+  width: 200px;
+  display: flex;
+  justify-content: center;
+  font-weight: 500;
+  font-size: 15px;
+  margin: 1rem;
+`;
